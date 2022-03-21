c06fb8ef2a708936179df7ca0039e24849324e0e
bshelling@gmail.com <bshelling@gmail.com>
2022-03-20 20:45:01 -0500

    Add login retry

--- src/fn.ts
+++ src/fn.ts
@@ -51,8 +51,8 @@ export const hashPw = async (pw: string) => {
 
 }
 
-export const genHash = () => {
-    return crypto.randomBytes(20).toString('hex')
+export const genHash = (bytes: number) => {
+    return crypto.randomBytes(bytes).toString('hex')
 }
 
 export const comparePw = async (hash: any, pw: string) => {
